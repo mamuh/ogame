6f8dfdf36f11db7db383d80495b3fd5817e1641b
Maxime Fournes <fournes.maxime@gmail.com>
2020-08-26 00:50:45 +0200

    Improved app, now initialises game and returns state.

--- game_backend/game_backend/app.py
+++ game_backend/game_backend/app.py
@@ -2,12 +2,27 @@
 
 from flask import Flask
 
+from game_backend.game import Game
+from game_backend.init_game import initialise_gamestate
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello World"
 
+
 @app.route("/get_state")
 def get_state():
-    pass
+    return game_state.to_json()
+
+
+if __name__ == "__main__":
+    game_state = initialise_gamestate()
+    game_thread = Game(game_state)
+
+    game_thread.start()
+
+    app.run()
