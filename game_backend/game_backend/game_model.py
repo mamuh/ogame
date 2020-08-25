6f7280186a97ed4df137109a32729a4b1869fa8a
Maxime Fournes <fournes.maxime@gmail.com>
2020-08-26 00:25:17 +0200

    Improved game model, added components

--- game_backend/game_backend/game_model.py
+++ game_backend/game_backend/game_model.py
@@ -1,33 +1,59 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
+from enum import Enum
 
 from dataclasses_jsonschema import JsonSchemaMixin
+
+
+class Resources(Enum):
+    Metal = 0
+
+
+@dataclass
+class Component(JsonSchemaMixin):
+    pass
+
+
+@dataclass
+class UpgradableComponent(Component, JsonSchemaMixin):
+    base_cost: float
+    upgrade_cost_factor: float
+
+
+@dataclass
+class ProducerComponent(Component, JsonSchemaMixin):
+    production_rate: Dict[Resources, float]
 
 
 @dataclass
 class Building(JsonSchemaMixin):
     pass
 
+
 @dataclass
 class Mine(Building, JsonSchemaMixin):
-    pass
+    producer_component: ProducerComponent
+    upgradable_component: UpgradableComponent
+
 
 @dataclass
 class Factory(Building, JsonSchemaMixin):
     pass
 
+
 @dataclass
 class Cannon(Building, JsonSchemaMixin):
     pass
+
 
 @dataclass
 class Planet(JsonSchemaMixin):
     id: str
     name: str
     size: int
-    buildings: List[Building]    
+    buildings: List[Building]
 
 
 @dataclass
@@ -45,4 +71,3 @@ class Player(JsonSchemaMixin):
 class GameState(JsonSchemaMixin):
     world: World
     players: Dict[str, Player]
-
